5c63cf1f4a9c47f0eec77ea63b9e4ca51396e391
phalat080 <57059698+phalat080@users.noreply.github.com>
2020-10-27 21:17:53 +0700

    finish packed and update promotion

--- js/promotion.js
+++ js/promotion.js
@@ -5,10 +5,14 @@ function promo(){
         show.style.display = "block";
         let show2 = document.getElementById('block2');
         show2.style.display = "block";
+        let show = document.getElementById('block3');
+        show.style.display = "block";
     }else{
         let show = document.getElementById('block1');
         show.style.display = "none";
         let show2 = document.getElementById('block2');
+        show2.style.display = "none";
+        let show2 = document.getElementById('block3');
         show2.style.display = "none";
     }
 }
