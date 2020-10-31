754ddec7e8bdf0470d139f065afd5acf6bc84046
Puwanut <friend2000f@gmail.com>
2020-10-31 12:19:07 +0700

    Update code navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,12 @@
 var navbar = document.getElementById("myTopnav");
-var items = (document.getElementById("navbar-items")).getElementsByTagName("span");
 var window_size = window.matchMedia("(max-width: 992px)");
 var banner = document.getElementsByClassName("banner")[0];
-toggle_menuline(window_size);
-window_size.addListener(toggle_menuline);
+change_banner_margintop(window_size);
+window_size.addListener(change_banner_margintop);
 
 function toggle_menu() {
     if (navbar.className === "topnav") {
-        navbar.className += " responsive";
+        navbar.className += " show";
         banner.style.marginTop = "-30px";
     } else {
         navbar.className = "topnav";
@@ -15,21 +14,10 @@ function toggle_menu() {
     }
 }
 
-function toggle_menuline(window_size) {
-    if (window_size.matches) {
-        setClassItems("")
-        if (navbar.className === "topnav responsive")
-            banner.style.marginTop = "-30px";
+function change_banner_margintop(window_size){
+    if (window_size.matches && navbar.className === "topnav show") {
+        banner.style.marginTop = "-30px";
     }
-    else {
-        setClassItems("menu-line")
+    else
         banner.style.marginTop = "80px";
-    }
-
-}
-
-function setClassItems(class_name) {
-    for (let i = 0; i < items.length; i++) {
-        items[i].className = class_name;
-    };
 }
