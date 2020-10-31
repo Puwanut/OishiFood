5f18c48940c1e3175c6b3fcbc6e14f48c9e81246
phalat080 <57059698+phalat080@users.noreply.github.com>
2020-10-31 22:19:31 +0700

    update promotion

--- js/promotion.js
+++ js/promotion.js
@@ -2,13 +2,22 @@ function promo(){
     let topic = document.getElementById('template-contactform-service').value;
     if (topic == 1){
         let show = document.getElementById('block1');
-        show.style.display = "block";
         let show2 = document.getElementById('block2');
+        show.classList.remove = "FadeOut"
+        show2.classList.remove = "FadeOut"
+        show.classList.add = "FadeIn"
+        show2.classList.add = "FadeIn"
+        show.style.display = "block";
         show2.style.display = "block";
     }else{
         let show = document.getElementById('block1');
-        show.style.display = "none";
         let show2 = document.getElementById('block2');
+        show.style
+        show.style.display = "none";
         show2.style.display = "none";
+        
     }
+}
+function timeFunction() {
+    setTimeout(promo(), 1500);
 }
