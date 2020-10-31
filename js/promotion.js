1cbc12fe34c02868f053edb0052856164a91524a
phalat080 <57059698+phalat080@users.noreply.github.com>
2020-10-31 21:23:53 +0700

    update promotion

--- js/promotion.js
+++ js/promotion.js
@@ -5,14 +5,10 @@ function promo(){
         show.style.display = "block";
         let show2 = document.getElementById('block2');
         show2.style.display = "block";
-        let show = document.getElementById('block3');
-        show.style.display = "block";
     }else{
         let show = document.getElementById('block1');
         show.style.display = "none";
         let show2 = document.getElementById('block2');
-        show2.style.display = "none";
-        let show2 = document.getElementById('block3');
         show2.style.display = "none";
     }
 }
