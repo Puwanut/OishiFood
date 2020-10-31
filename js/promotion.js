94ebba79b080aafd082a094df5b25882557df014
Puwanut <friend2000f@gmail.com>
2020-10-31 23:12:16 +0700

    Revert "Add animation fade in out"
    This reverts commit 8a93beb12a82ae30cf5e7c48d8bdc682bd408917.

--- js/promotion.js
+++ js/promotion.js
@@ -1,26 +1,23 @@
-let show = document.getElementById('block1');
-let show2 = document.getElementById('block2');
-
 function promo(){
     let topic = document.getElementById('template-contactform-service').value;
-    if (topic <= 1){
-        show.classList.remove("FadeOut");
-        show2.classList.remove("FadeOut");
-
-        show.classList.add("FadeIn");
-        show2.classList.add("FadeIn");
+    if (topic == 1){
+        let show = document.getElementById('block1');
+        let show2 = document.getElementById('block2');
+        show.classList.remove = "FadeOut"
+        show2.classList.remove = "FadeOut"
+        show.classList.add = "FadeIn"
+        show2.classList.add = "FadeIn"
         show.style.display = "block";
         show2.style.display = "block";
     }else{
-        show.classList.remove("FadeIn");
-        show2.classList.remove("FadeIn");
-
-        show.classList.add("FadeOut");
-        show2.classList.add("FadeOut");
-        setTimeout(displayNone, 300);
+        let show = document.getElementById('block1');
+        let show2 = document.getElementById('block2');
+        show.style
+        show.style.display = "none";
+        show2.style.display = "none";
+        
     }
 }
-function displayNone() {
-    show.style.display = "none";
-    show2.style.display = "none";
+function timeFunction() {
+    setTimeout(promo(), 1500);
 }
