047f5eb16899a9bdeb311c24f39cb7db74712787
boss26974 <56310456+boss26974@users.noreply.github.com>
2020-11-09 21:43:08 +0700

    Update contact_us.js

--- js/contact_us.js
+++ js/contact_us.js
@@ -1,8 +1,34 @@
+var numName = 0;
 function contactUs(){
-    name = document.getElementsByClassName("firstname")[0].Value;
-    nameLong = name.length;
-    if(nameLong == 0){
-        alert(nameLong);
+    name = document.getElementsByClassName("firstname")[0].value.length;
+    email = document.getElementsByClassName("email")[0].value.length;
+    in_email = document.getElementsByClassName("email")[0].value;
+    message = document.getElementsByClassName("message")[0].value.length;
+    if(name <= 0){
+        document.getElementsByClassName("firstname")[0].style.border = "solid red 2px";
+    }
+    else{
+        document.getElementsByClassName("firstname")[0].style.border = "solid #dddddd 2px";
+    }
+    if(email <= 0){
+        document.getElementsByClassName("email")[0].style.border = "solid red 2px";
+    }
+    else{
+        for(var i = 0; i < email; i++){
+            if((in_email.charCodeAt(i) == 64) && (in_email.charAt(i) != null) && (in_email.charAt(i) != null)){
+                document.getElementsByClassName("email")[0].style.border = "solid #dddddd 2px";
+                break;
+            }
+            else{
+                document.getElementsByClassName("email")[0].style.border = "solid red 2px";
+            }
+        }
+    }
+    if(message <= 0){
+        document.getElementsByClassName("message")[0].style.border = "solid red 2px";
+    }
+    else{
+        document.getElementsByClassName("message")[0].style.border = "solid #dddddd 2px";
     }
     return false;
 }
