e4f908af6095d810e59f5dad6b4c99495fa7ef72
phalat080 <57059698+phalat080@users.noreply.github.com>
2020-11-15 22:04:41 +0700

    Update promotion

--- js/promotion.js
+++ js/promotion.js
@@ -3,6 +3,7 @@ let ref = params.get("ref");
 let topic = document.getElementById('template-contactform-service');
 let show = document.getElementById('block1');
 let show2 = document.getElementById('block2');
+let show3 = document.getElementById('block3');
 
 switch (ref) {
     case "restaurant": topic.value = 1; break;
@@ -16,21 +17,27 @@ function promo(){
     if (topic <= 1){
         show.classList.remove("FadeOut");
         show2.classList.remove("FadeOut");
+        show3.classList.remove("FadeOut");
 
         show.classList.add("FadeIn");
         show2.classList.add("FadeIn");
+        show3.classList.add("FadeIn");
         show.style.display = "block";
         show2.style.display = "block";
+        show3.style.display = "block";
     }else{
         show.classList.remove("FadeIn");
         show2.classList.remove("FadeIn");
+        show3.classList.remove("FadeIn");
 
         show.classList.add("FadeOut");
         show2.classList.add("FadeOut");
+        show3.classList.add("FadeOut");
         setTimeout(displayNone, 300);
     }
 }
 function displayNone() {
     show.style.display = "none";
     show2.style.display = "none";
+    show3.style.display = "none";
 }
